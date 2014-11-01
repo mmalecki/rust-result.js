61a5d4494541e12e32e5c334b1af668e9659e93a
Raynos <raynos2@gmail.com>
2014-11-01 15:59:15 -0700

    Remove RESULT_UUID and associated global
    In the implementation we can just check the only two options
        and thus remove a global variable.

--- index.js
+++ index.js
@@ -1,49 +1,48 @@
 var Individual = require('individual');
 
 var VERSION_KEY = '1';
-var RESULT_CACHE_KEY = '__RUST_RESULT_RESULT_UUID@' + VERSION_KEY;
 var ERROR_CACHE_KEY = '__RUST_RESULT_ERROR_UUID@' + VERSION_KEY;
 var OK_CACHE_KEY = '__RUST_RESULT_OK_UUID@' + VERSION_KEY;
 
-var RESULT_UUID = Individual(RESULT_CACHE_KEY, fakeUUID('Result'));
 var ERROR_UUID = Individual(ERROR_CACHE_KEY, fakeUUID('Error'));
 var OK_UUID = Individual(OK_CACHE_KEY, fakeUUID('Ok'));
 
 function Ok(v) {
   this.v = v
 
-  this[RESULT_UUID] = true;
   this[OK_UUID] = true;
 }
 
 function Err(err) {
   this.err = err
 
-  this[RESULT_UUID] = true;
   this[ERROR_UUID] = true;
 }
 
 function createOk(v) {
-  if (isObject(v) && RESULT_UUID in v) {
-    if (isObject(v) && OK_UUID in v) return v.v
+  if (isObject(v) && OK_UUID in v) {
+    return v.v
+  } else if (isObject(v) && ERROR_UUID in v) {
     return undefined
+  } else {
+    return new Ok(v)
   }
-  return new Ok(v)
 }
 
 function createError(err) {
-  if (isObject(err) && RESULT_UUID in err) {
-    if (isObject(err) && ERROR_UUID in err) return err.err
+  if (isObject(err) && ERROR_UUID in err) {
+    return err.err
+  } else if (isObject(err) && OK_UUID in err) {
     return undefined
+  } else {
+    return new Err(err)
   }
-  return new Err(err)
 }
 
 module.exports = {
   Ok: createOk,
   Err: createError
 }
-
 
 function fakeUUID(word) {
   return 'rust-result:' + word + ':' +
