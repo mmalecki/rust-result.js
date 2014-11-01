6f595f60f7b66b724ef7b36ec8454bc6207140ed
Raynos <raynos2@gmail.com>
2014-11-01 15:48:11 -0700

    Remove instanceof and replace with uuids
    `instanceof` checks break across the node_modules boundary.
    
    If I import a library that returns a `Result` but have a
        different copy of `Result` for checking then it uses
        then there are two constructors and `instanceof` breaks.
    
    The only real work around is to "brand" objects with a UUID.
        This UUID must be globally cached internally against the
        major version number.
    
    I updated the code to create approximate uuids that are not
        random but random enough and cheap (loc) to create.
    
    I then use the `Individual` module to create a globally cached
        singleton UUID for each of the three classes.
    
    I use these brands for checking in the Result.Ok and Result.Err
        functions.

--- index.js
+++ index.js
@@ -1,28 +1,39 @@
-function Result(result) {
-  this.result = result
-}
+var Individual = require('individual');
+
+var VERSION_KEY = '1';
+var RESULT_CACHE_KEY = '__RUST_RESULT_RESULT_UUID@' + VERSION_KEY;
+var ERROR_CACHE_KEY = '__RUST_RESULT_ERROR_UUID@' + VERSION_KEY;
+var OK_CACHE_KEY = '__RUST_RESULT_OK_UUID@' + VERSION_KEY;
+
+var RESULT_UUID = Individual(RESULT_CACHE_KEY, fakeUUID('Result'));
+var ERROR_UUID = Individual(ERROR_CACHE_KEY, fakeUUID('Error'));
+var OK_UUID = Individual(OK_CACHE_KEY, fakeUUID('Ok'));
 
 function Ok(v) {
   this.v = v
+
+  this[RESULT_UUID] = true;
+  this[OK_UUID] = true;
 }
-Ok.prototype = new Result()
 
 function Err(err) {
   this.err = err
+
+  this[RESULT_UUID] = true;
+  this[ERROR_UUID] = true;
 }
-Err.prototype = new Result()
 
 function createOk(v) {
-  if (v instanceof Result) {
-    if (v instanceof Ok) return v.v
+  if (isObject(v) && RESULT_UUID in v) {
+    if (isObject(v) && OK_UUID in v) return v.v
     return undefined
   }
   return new Ok(v)
 }
 
 function createError(err) {
-  if (err instanceof Result) {
-    if (err instanceof Err) return err.err
+  if (isObject(err) && RESULT_UUID in err) {
+    if (isObject(err) && ERROR_UUID in err) return err.err
     return undefined
   }
   return new Err(err)
@@ -31,4 +42,17 @@ function createError(err) {
 module.exports = {
   Ok: createOk,
   Err: createError
+}
+
+
+function fakeUUID(word) {
+  return 'rust-result:' + word + ':' +
+    Math.random().toString(32).slice(2) + ':' +
+    Math.random().toString(32).slice(2) + ':' +
+    Math.random().toString(32).slice(2) + ':' +
+    Math.random().toString(32).slice(2) + ':';
+}
+
+function isObject(o) {
+  return typeof o === 'object' && o !== null;
 }
